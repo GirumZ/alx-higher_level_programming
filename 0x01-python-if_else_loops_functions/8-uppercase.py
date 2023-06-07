33308ddbbca5725eae6cb407950df02ec5d23fa5
GirumZ <girum.zebe@gmail.com>
2023-06-06 18:17:15 -0700

    Create: a program named 8-uppercase that converts a string to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            diff = 32
+        else:
+            diff = 0
+        print("{:c}".format(ord(str[i]) - diff), end='')
+    print("")
