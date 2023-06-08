49bf677d22d67e2a1f11834b99f1cefff08166cb
GirumZ <girum.zebe@gmail.com>
2023-06-08 04:31:30 -0700

    Create: a program named 2-args.py that prints elements of argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    number = len(sys.argv)
+    if number == 2:
+        text = 'argument'
+    else:
+        text = 'arguments'
+    print("{} {}".format(number - 1, text))
+    for i in range(1, number):
+        print("{}: {}".format(i, sys.argv[i]))
