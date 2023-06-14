92be6182a51d0c7a99084a3718c7d7e2b419366f
GirumZ <girum.zebe@gmail.com>
2023-06-14 15:04:55 -0700

    [Create]:a function named weighted_average that calculates weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    total = 0
+    total_weight = 0
+    for score, weight in my_list:
+        total += score * weight
+        total_weight += weight
+    average = total / total_weight
+    return (average)
