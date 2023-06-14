352b52929c26c66c91e68eb9da0feeb4976e86a5
GirumZ <girum.zebe@gmail.com>
2023-06-14 10:49:24 -0700

    [Fix] made the code pycodestyle complient

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     copy_matrix = matrix.copy()
     for i in range(len(matrix)):
-        copy_matrix[i] = list(map(lambda x : x**2, matrix[i]))
+        copy_matrix[i] = list(map(lambda x: x**2, matrix[i]))
     return (copy_matrix)
