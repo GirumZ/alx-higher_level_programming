d8baeddb4c57c9c9d9329eaa852bfa85343d54a2
GirumZ <girum.zebe@gmail.com>
2023-06-27 18:36:39 -0700

    [Create]: a py class with size attribute and different methods

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Defination of class 'Square' with privet instance attribute"""
+
+
+class Square:
+    """Class Square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Inits Square with privet instance attribute 'size'
+
+        Args:
+            size: Size of the square with integer value
+
+        Raises:
+            TypeError: When size is not integer
+            ValueError: When size is negative
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of a square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """ Returns the size value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size value of the square
+
+        Args:
+            value: Size of the square to be set with integer value
+
+        Raises:
+            TypeError: When size is not integer
+            ValueError: When size is negative
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """ Prints the square with the # character"""
+        if not self.__size:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
