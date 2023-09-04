8cdd72d169d7ea2e0ad1776294618f7a0c23edfd
GirumZ <girum.zebe@gmail.com>
2023-09-04 12:16:43 -0700

    [bug fix]: made the files executable as requasted

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/pyhton3
-""" This module creates an empty class Rectangle that defines a rectangle"""
+""" class module creates an empty class Rectangle that defines a rectangle"""
+
+
 class Rectangle:
     """ An empty class called Rectangle"""
     pass
