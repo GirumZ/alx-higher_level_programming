51fc69595d4c65e5f2f6b9df019e643ae6c42e02
GirumZ <girum.zebe@gmail.com>
2023-09-11 05:05:23 -0700

    [Bug fix]: Handled whitespace issue for pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,30 +2,8 @@
 """ A module that defines a class called 'BaseGeometry'"""
 
 
-class BaseGeometry:
-    """ A class called BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    def area(self):
-        """ A public instance method that computes the area of an instance
-        Raises:
-            Excepsion: Always
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ A public instance method that validates a value
-
-        Args:
-            name: Name
-            value: Value
-        Raises:
-            TypeError: if value is not integer
-            ValueError: if value is less or equal to 0
-        """
-        if not type(value) == int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """ A Rectangle class that is a subclass of BaseGeometry class"""
