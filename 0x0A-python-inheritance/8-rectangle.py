a175062a8b5ef30ae4639ee2f0840907f354e678
GirumZ <girum.zebe@gmail.com>
2023-09-11 04:57:07 -0700

    [Create]: A python module that defines a Rectangle class in a BaseGeometry class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" A module that defines a class called 'BaseGeometry'"""
+
+
+class BaseGeometry:
+    """ A class called BaseGeometry"""
+
+    def area(self):
+        """ A public instance method that computes the area of an instance
+        Raises:
+            Excepsion: Always
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ A public instance method that validates a value
+
+        Args:
+            name: Name
+            value: Value
+        Raises:
+            TypeError: if value is not integer
+            ValueError: if value is less or equal to 0
+        """
+        if not type(value) == int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """ A Rectangle class that is a subclass of BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """ A creator method that initializes a class instance
+
+        Args:
+            width: the width of a Rectangle instance
+            height: the height of a Rectangle instance
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
