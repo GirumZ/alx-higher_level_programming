14362629daa31bddf5bc1966c8aebec18eb55975
GirumZ <girum.zebe@gmail.com>
2023-10-17 08:03:18 -0700

    [Create]: A script that display the record of second_table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- A script that lists all records of the table second_table
+-- Result will be orderd by score and contains score and name
+SELECT score, name FROM second_table ORDER BY score DESC;
