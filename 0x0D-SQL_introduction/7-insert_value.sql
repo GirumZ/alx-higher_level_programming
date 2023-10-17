31dd00e4ce30eb39e963233a5a8ef80628e2f3da
GirumZ <girum.zebe@gmail.com>
2023-10-17 06:14:31 -0700

    [Create]: A script that adds data to a mysql database table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- A script that adds data to a table in mysql database
+-- INSERT INTO(). . .VALUES() is used to  add data in a table
+INSERT INTO first_table(id, name)
+VALUES (89, 'Best School');
