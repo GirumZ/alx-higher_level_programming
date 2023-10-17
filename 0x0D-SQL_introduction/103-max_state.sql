56c6a2f93a1a1e6a5ba4d547bcda4624bdc991e2
GirumZ <girum.zebe@gmail.com>
2023-10-17 13:04:00 -0700

    [Bug fix]: Cwqorrected spelling errors

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- A script that displays the max temperature of each state
 -- hbtn_0c_0 is given
 SELECT state, MAX(value) AS max_temp
-FROM tempratures
+FROM temperatures
 GROUP BY state
 LIMIT 3;
