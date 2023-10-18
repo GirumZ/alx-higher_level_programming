ff51eafb9fa3116d6e1876ed7bf2b5c7ff38c313
GirumZ <girum.zebe@gmail.com>
2023-10-18 13:56:39 -0700

    [Create]: A script that lists all privileges to a user

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- A script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- A querie for user1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- A querie for user2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
