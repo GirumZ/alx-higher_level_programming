bcf150c3b26741052d1bfec525cfb1cfe15e99a9
GirumZ <girum.zebe@gmail.com>
2023-10-19 08:37:04 -0700

    [Bug fix]: Removed unwanted whitespace on the comments

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
- -- A script that creates the table force_name on your MySQL server
+-- A script that creates the table force_name on your MySQL server
 -- Create a force_name table
-Create TABLE IF NOT EXISTS 'force_name' (
-	'id' INT,
-	'name' VARCHAR(256) NOT NULL
+Create TABLE IF NOT EXISTS force_name (
+	id INT,
+	name VARCHAR(256) NOT NULL
 	);
