1169d98dbdee893dcf08cb666bee219c30988898
GirumZ <girum.zebe@gmail.com>
2023-10-19 09:06:36 -0700

    [Bug fix]: Removed unwanted white spacces

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,12 @@
 -- A script that creates the database hbtn_0d_usa and the table cities
 -- Create the database
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Use database
 USE hbtn_0d_usa;
---Create table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+-- Create table
+CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES state(id));
+	FOREIGN KEY (state_id) REFERENCES states(id));
