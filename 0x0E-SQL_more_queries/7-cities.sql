6aa57359093872e8401277d356e20123020dddb9
GirumZ <girum.zebe@gmail.com>
2023-10-18 19:55:22 -0700

    [Bug fix]: A added the use querie for the database

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- A script that creates the database hbtn_0d_usa and the table cities
 -- Create the database
 CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+-- Use database
+USE hbtn_0d_usa;
 --Create table
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
