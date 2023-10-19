be61e5ab4449af5a0a2facbc2b1a717927dad558
GirumZ <girum.zebe@gmail.com>
2023-10-18 19:30:58 -0700

    [Create]: A script that creates the table force_name on your MySQL server

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+ -- A script that creates the table force_name on your MySQL server
+-- Create a force_name table
+Create TABLE IF NOT EXISTS 'force_name' (
+	'id' INT,
+	'name' VARCHAR(256) NOT NULL
+	);
