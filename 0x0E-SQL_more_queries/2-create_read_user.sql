3679bacdaeeba1b175b5484871e50c25e5024392
GirumZ <girum.zebe@gmail.com>
2023-10-18 19:21:27 -0700

    [Create]: A script that creates the database hbtn_0d_2 and the user user_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- A script that creates the database hbtn_0d_2 and the user user_0d_2
+-- First create the database
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+--Create a user 
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant privilege
+GRANT SELECT ON 'htbn_0d_2'.* TO 'user_0d_2'@'localhost';
