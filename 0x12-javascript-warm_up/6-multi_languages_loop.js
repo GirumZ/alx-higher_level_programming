8ec246a9cb52fec5a0c01141e75f45dda12d9846
GirumZ <girum.zebe@gmail.com>
2023-11-13 11:38:36 -0800

    [Bug fix]: corrected spelling errors

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const str_array = ['C is fun', 'Python is cool', 'JavaScript is amaizing']
-for (let i = 0; i < str_array.length; i++) {
-  console.log(str_array[i]);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
 }
