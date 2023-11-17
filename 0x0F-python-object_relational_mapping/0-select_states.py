bfbe97c3e828dd0043508d99bf7254c03ae12751
GirumZ <girum.zebe@gmail.com>
2023-11-16 23:30:06 -0800

    [Bug fix]: Corrected for pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,15 @@ A script that lists all states from the database hbtn_0e_0_usa
 import sys
 import MySQLdb
 
+
 def main():
     """ Interacts with a database"""
+
+
 args = sys.argv
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=args[1], passwd=args[2], db=args[3], charset="utf8")
+conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
+                       passwd=args[2], db=args[3], charset="utf8")
 cur = conn.cursor()
 query = "SELECT * FROM states ORDER BY id ASC"
 cur.execute(query)
