e0e14e21a322fee87a807ac3eda780b6dfc844ec
GirumZ <girum.zebe@gmail.com>
2023-11-16 23:49:42 -0800

    [Bug fix]: fixed the execute function

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,8 +17,7 @@ args = sys.argv
 conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
                        passwd=args[2], db=args[3], charset="utf8")
 cur = conn.cursor()
-query = "SELECT * FROM states ORDER BY id ASC"
-cur.execute(query)
+cur.execute("SELECT * FROM states ORDER BY id ASC")
 rows = cur.fetchall()
 
 for row in rows:
