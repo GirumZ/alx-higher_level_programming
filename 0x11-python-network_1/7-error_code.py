e674e3a50e811115734a8514d66cc1e49cc8d901
GirumZ <girum.zebe@gmail.com>
2023-12-01 23:11:40 +0300

    [Bug fix]: Spelling error fixed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
 
     req = requests.get(sys.argv[1])
     if req.status_code >= 400:
-        print("Error code: {}".format(req.status_xode))
+        print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
